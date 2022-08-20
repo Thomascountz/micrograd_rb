fb37471644510a82c4472cf9f843e85fc249b42b
Thomas Countz <thomascountz@gmail.com>
2022-08-20 22:19:29 +0200

    alter mlp API to take in one list of layer sizes

--- lib/mlp.rb
+++ lib/mlp.rb
@@ -1,19 +1,18 @@
 require "./lib/layer"
 
 class MLP
-  attr_reader :input_size, :output_sizes, :layers
+  attr_reader :layers
 
-  def initialize(input_size:, output_sizes:)
-    @input_size = input_size
-    @output_sizes = output_sizes
+  def initialize(layer_sizes:)
     @layers = []
-    ([input_size] + output_sizes).each_cons(2) do |input_size, output_size|
+    @input_size = layer_sizes.first
+    layer_sizes.each_cons(2) do |input_size, output_size|
       @layers << Layer.new(input_size: input_size, output_size: output_size)
     end
   end
 
   def call(inputs:)
-    raise ArgumentError.new("MLP expects #{input_size} inputs. Got #{inputs.size}.") unless inputs.size == input_size
+    raise ArgumentError.new("MLP expects #{@input_size} inputs. Got #{inputs.size}.") unless inputs.size == @input_size
     @layers.each do |layer|
       inputs = layer.call(inputs: inputs)
     end
--- spec/mlp_spec.rb
+++ spec/mlp_spec.rb
@@ -2,25 +2,29 @@ require "./lib/mlp"
 
 RSpec.describe MLP do
   it "initializes a layer for each pair of input and output sizes" do
-    mlp = MLP.new(input_size: 3, output_sizes: [4, 4, 1])
+    mlp = MLP.new(layer_sizes: [3, 4, 4, 1])
     expect(mlp.layers.length).to eq(3)
-    expect(mlp.layers.first.input_size).to eq(3)
-    expect(mlp.layers.last.output_size).to eq(1)
+    expect(mlp.layers[0].input_size).to eq(3)
+    expect(mlp.layers[0].output_size).to eq(4)
+    expect(mlp.layers[1].input_size).to eq(4)
+    expect(mlp.layers[1].output_size).to eq(4)
+    expect(mlp.layers[2].input_size).to eq(4)
+    expect(mlp.layers[2].output_size).to eq(1)
   end
 
   describe "#call" do
     it "returns the value of the final layer" do
-      mlp = MLP.new(input_size: 3, output_sizes: [4, 4, 1])
+      mlp = MLP.new(layer_sizes: [3, 4, 4, 1])
       expect(mlp.call(inputs: [1, 1, 1])).to a_kind_of(Value)
     end
 
     it "returns a list of values if the output size is > 1" do
-      mlp = MLP.new(input_size: 3, output_sizes: [4, 4, 3])
+      mlp = MLP.new(layer_sizes: [3, 4, 4, 3])
       expect(mlp.call(inputs: [1, 1, 1])).to be_a(Array).and all(be_a(Value))
     end
 
     it "raises an error if the number of inputs doesn't match input size" do
-      mlp = MLP.new(input_size: 3, output_sizes: [4, 4, 1])
+      mlp = MLP.new(layer_sizes: [3, 4, 4, 3])
       expect { mlp.call(inputs: [1]) }.to raise_error(ArgumentError)
     end
   end
