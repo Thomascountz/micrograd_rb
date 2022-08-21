63c4d4fc9641e7b572c77ac06f32ece82ebae6e0
Thomas Countz <thomascountz@gmail.com>
2022-08-21 16:05:18 +0200

    WIP experiment with highlighting parameter nodes

--- mermaid/graph.rb
+++ mermaid/graph.rb
@@ -1,18 +1,21 @@
 module Mermaid
   class Graph
-    GRAPH = "graph"
-    LEFT_TO_RIGHT = "LR"
+    GRAPH = "graph".freeze
+    LEFT_TO_RIGHT = "LR".freeze
+    GREEN_FILL = "classDef green fill:#3ddc97;".freeze
 
     # Outputs a mermaid.js graph string for a given root node
     # Node Format: node.label.upcase[node.label.upcase | (node.gradient) | node.data]
     # Operation Format: node.label.upcase + operation_to_s(node.operation)[(node.operation)]
     # Edge Format: node.label.upcase + operation_to_s(node.operation) --> node.label.upcase
-    def self.output(root)
-      out = GRAPH + " " + LEFT_TO_RIGHT + "\n"
+    def self.output(root, **opts)
+      out = GRAPH + " " + LEFT_TO_RIGHT + "\n" + GREEN_FILL + "\n"
 
       trace = trace(root)
+      labeled = trace[:nodes].map { |n| n.label.to_s.upcase }.reject { |n| n.empty? }
 
       trace[:nodes].each do |node|
+        node.label = node.object_id.to_s if node.label.nil?
         out << node_string(node) # C["C | (1.000) | 2.000"]
         if node.operation
           out << operation_node_string(node, node.operation) # Cplus(("+"))
@@ -24,11 +27,10 @@ module Mermaid
         out << operand_edge_string(edge) # A --> Cplus, B --> Cplus
       end
 
-      out
+      out << "class #{labeled.join(",")} green;"
     end
 
     def self.trace(root, nodes = [], edges = [])
-      root.label = root.object_id.to_s if root.label.nil?
       if !nodes.include?(root)
         nodes << root
         root.children.each do |child|
