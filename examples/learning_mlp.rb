5e959d9c67338c84d1bfbaf3106f3ff0d8e74c20
thomascountz <thomascountz@gmail.com>
2022-08-21 18:23:17 +0200

    ensure we `zero_grad()` before backprop
    see: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch

--- examples/learning_mlp.rb
+++ examples/learning_mlp.rb
@@ -11,20 +11,22 @@ ys = [1.0, -1.0, -1.0, 1.0] # desired targets
 
 mlp = MLP.new(layer_sizes: [3, 4, 4, 1])
 loss = nil
-20.times do |i|
+50.times do |i|
   # forward pass
   predictions = xs.map { |x| mlp.call(inputs: x) }
   loss = predictions.zip(ys).sum { |ypred, y| (ypred - y)**2 }
 
   # backward pass
+  # First, set all of the parameter gradients to 0 so that we don't accumulate gradients from previous iterations
+  mlp.parameters.each { |p| p.gradient = 0.0 }
   loss.backpropagate
 
   # update weights
-  mlp.parameters.each { |param| param.data += -0.05 * param.gradient }
+  mlp.parameters.each { |param| param.data += -0.1 * param.gradient }
 
   puts "Iteration #{i}: loss = #{loss.data}"
 end
 
-mlp.parameters.each_with_index { |param, i| param.label = "param#{i}"}
+mlp.parameters.each_with_index { |param, i| param.label = "param#{i}" }
 
 Mermaid::GraphHTMLWriter.write(loss, "trained_mlp.html")
